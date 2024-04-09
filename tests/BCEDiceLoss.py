917a73f44d5052845cf619ba6cd55654d025bb94
claycrews2002 <claycrews2002@gmail.com>
2024-04-09 08:55:57 -0400

    ultra lightm-unet results

---
+++ tests/BCEDiceLoss.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+
+class BCELoss(nn.Module):
+    def __init__(self):
+        super(BCELoss, self).__init__()
+        self.bceloss = nn.BCELoss()
+
+    def forward(self, pred, target):
+        size = pred.size(0)
+        pred_ = pred.view(size, -1)
+        target_ = target.view(size, -1)
+
+        return self.bceloss(pred_, target_)
+
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, pred, target):
+        smooth = 1
+        size = pred.size(0)
+
+        pred_ = pred.view(size, -1)
+        target_ = target.view(size, -1)
+        intersection = pred_ * target_
+        dice_score = (2 * intersection.sum(1) + smooth)/(pred_.sum(1) + target_.sum(1) + smooth)
+        dice_loss = 1 - dice_score.sum()/size
+
+        return dice_loss
+
+
+class BceDiceLoss(nn.Module):
+    def __init__(self, wb=1, wd=1):
+        super(BceDiceLoss, self).__init__()
+        self.bce = BCELoss()
+        self.dice = DiceLoss()
+        self.wb = wb
+        self.wd = wd
+
+    def forward(self, pred, target):
+        bceloss = self.bce(pred, target)
+        diceloss = self.dice(pred, target)
+
+        loss = self.wd * diceloss + self.wb * bceloss
+        return loss
