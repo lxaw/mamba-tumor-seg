275d94ca2f83d99a03faccdafe5cb65ea90f9ae8
lxaw <whalenlex@gmail.com>
2024-04-26 10:51:39 -0400

    finalized

--- final_upload/tests/calculate_iou.py
+++
@@ -1,9 +0,0 @@
-import torch 
-
-def calculate_iou(pred_mask, true_mask):
-
-    intersection = torch.logical_and(pred_mask, true_mask).sum()
-    union = torch.logical_or(pred_mask, true_mask).sum()
-
-    iou = intersection / union if union != 0 else 0.0
-    return iou
